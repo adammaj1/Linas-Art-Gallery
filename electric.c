1ad98f67d38db3852ab3e0fc3d771f014cc2b539
adammaj1@o2.pl <adammaj1@o2.pl>
2018-01-05 19:51:30 +0100

    electric new code

--- electric.c
+++ electric.c
@@ -4,19 +4,93 @@
 
    http://linas.org/art-gallery/escape/ray.html
    image : 
-   http://linas.org/art-gallery/escape/electric.gif
+  http://linas.org/art-gallery/escape/landing.gif
    
    
    "Gradient of the potential
 
-   The gradient is more interesting. The gradient 2Df always points 'uphill':  
+The gradient is more interesting. The gradient 2Df always points 'uphill':  
 
-   2Df = f zn Dzn / |zn|2 log |zn|
+2Df = f zn Dzn / |zn|2 log |zn|
 
-   The picture below shows the modulus of the gradient.  Of course, it resembles a coronal discharge.  
-   If one were to cut out a metal plate in the shape of an M-set, apply a high voltage to it, 
-   it is exactly this that you would see, because it is the areas of high electric field (gradient) where the coronal ionization will occur. 
-   Maybe make a chic lightning rod for the barn. " Linas Vepstas
+The picture below shows the modulus of the gradient.  Of course, it resembles a coronal discharge.  
+If one were to cut out a metal plate in the shape of an M-set, apply a high voltage to it, 
+it is exactly this that you would see, because it is the areas of high electric field (gradient) where the coronal ionization will occur. 
+Maybe make a chic lightning rod for the barn. " Linas Vepstas
+
+
+OK. so there are three mistakes.   Lets start with the simple one:  carg returns [-pi, pi] you want zero to one, so
+
+@@ -270,7 +283,9 @@ int ComputeAndSavePixelColor(int iX, int iY){
+   C = give_c(iX, iY);
+    
+   g = GiveGradient(C);
+-  ga = carg(g); 
++
++  // ga = (carg(g) + M_PI)/(2.0*M_PI);
++  ga = 1.0 - GiveTurn(g);
+   
+   // index of 1D memory array
+   k = f(iX, iY);  
+
+
+The reason for 1-GiveTurn(g) is because the gradient should point uphill, not downhill.  Thus a minus sign.  
+
+Next the gradient is a vector (a complex number), but in your code it was declared as a real!!  Fix that:
+
+@@ -209,7 +209,7 @@ double complex give_c(int iX, int iY){
+ // http://linas.org/art-gallery/escape/ray.html
+ double complex GiveGradient(double complex C ){
+ 
+-  double g; // gradient
++  double complex g; // gradient
+   int i; // iteration
+   double complex Z  = 0.0; // initial value for iteration Z0
+   double complex dZ = 1.0; // derivative with respect to c
+
+
+Next, the gradient formula in the code was not quite right; you wrote cabs(Z*dZ) but that's wrong; there is no cabs. Also Z should be Zbar, the complex conjugate of Z.   So it should have been conj(Z)*dZ
+
+@@ -237,12 +237,25 @@ double complex GiveGradient(double complex C ){
+   //m = m/IterationMax; // normalize = map to [0,1]
+   
+   
+-  // the Douady-Hubbard potential is just f = e-m log2 = 2-m
++  // the Douady-Hubbard potential is just f = exp(-m log2) = 2^{-m}
++  // where ^ means superscript:  so a^b = a raised to power b.
++  // This is TeX/LaTex notation (e.g. wikipedia math formulas).
+   double potential = pow(2,-m);
+-  // 2Df = m zn Dzn / |zn|2 log |zn|
++
++  // 2Df = 2^{-m} (zbar)_n Dz_n / |z_n|^2 log |z_n|
++  // where zbar is the complex conjugate of z.
++  // This is because:...
++  // |z|^2 = zbar z
++  // and thus d|z| = d sqrt(zbar z) = 0.5 d(zbar z) / sqrt(zbar z)
++  // = 0.5 zbar dz / sqrt(zbar z) = 0.5 zbar dz / |z|
++  //
++  // notice that d zbar = 0  which is why d(zbar z) = zbar dz
++  // this is because zbar only depends on cbar and d cbar = 0 by
++  // orthogonality. So by defintion dc = 1
+  
+-  g = potential*cabs(Z*dZ)/(cabsZ*cabsZ*log(cabsZ));
+-  
++  double complex Zbar = conj(Z);
++  g = potential * Zbar * dZ / (cabsZ*cabsZ*log(cabsZ));
++
+   return g;
+ }
+ 
+You would have caught these mistakes if you tried to compute the gradient yourself ... you should do it, its not that hard.
+
+Attached is the final code, and the image.  Your new colormap is different and ugly-ish. The line between red-and-yellow is much much worse than before.  the red-yellow discontinuity is ... confusing, annoying. .. to me, at least.
+
+Merry Christmas and Happy 2018
+
+--linas
+
+
+
    
   
    c console program:
@@ -41,14 +115,14 @@
    https://stackoverflow.com/questions/6418807/how-to-work-with-complex-numbers-in-c
    complex numbers are built in type 
  
-   --------------
-   formated with emacs
+ --------------
+ formated with emacs
    -------------
    to compile : 
 
  
  
-   gcc electric.c -lm -Wall 
+   gcc landing.c -lm -Wall 
  
  
    ./a.out
@@ -56,37 +130,14 @@
    
    to convert to png using ImageMagic
 
-   convert electric.ppm electric.png  
+   convert landing.ppm landing.png  
 
 
 
    ----------------------
-   git add  README.md electric.png electric.c
-   git commit -m " electric"
-   git push -u origin master
-
-   ----------------
-   -----------------------------------------
-
-   No errors. End of setup 
-   File electric_frac2.ppm saved. 
-   Parameter plane ( c plane) with Mandelbrot set for complex quadratic polynomial fc(z) = z^2 + c
-   Rectangle part of 2D parameter plane: corners: 
-   CxMin = -2.300000;   CxMax = 0.700000;  CyMin = -1.500000; CyMax = 1.500000 
-   center and radius: 
-   CenterX = -0.800000;   CenterY = 0.000000;  radius = 1.500000
-   Mag = zoom = 0.666667
-   PixelWidth = 0.003000 and PixelHeight =0.003000
-   Escape Radius = 300.000000
-   Iteration Max = 88
-  
-   i = cabs(gradient) * 255   : 
-   i_max = 254
-   i_min = -2147483648
-   gradient : 
-   gmax = 5776150.171435
-   gmin = 0.000000
-
+   git add  landing.png landing.c
+   git commit -m " landing"
+   push -u origin master
 
 
 
@@ -106,8 +157,8 @@
  
 /* screen ( integer) coordinate */
 
-const int iWidth  = 1000; 
-const int iHeight = 1000;
+const int iWidth  = 5000; 
+const int iHeight = 5000;
 
 
 /* world ( double) coordinate = parameter plane*/
@@ -132,6 +183,9 @@ const int IterationMax=88;
 /* bail-out value , radius of circle ;  */
 const double EscapeRadius=300.0;
 double log_2; // = log(2.0);
+double TwoPi=2.0*M_PI;
+
+
        
 // memmory virtual 1D array 
 unsigned char *data;       
@@ -169,7 +223,7 @@ void  GiveLinasColor(double position , int k, unsigned char c[])
     c[k] = 0;
     c[k+1] = (unsigned char) (i-60)*3;
     c[k+2] = (unsigned char) (120-i)*3;
-    // return 0;
+   // return 0;
   }
   
   /* ramp from green to yellow */
@@ -181,20 +235,20 @@ void  GiveLinasColor(double position , int k, unsigned char c[])
    
   }
   /* ramp from yellow to red (pink) */
-  // if (i>=180 && i<iMax) {
-  //    c[k] = (unsigned char) (210 + (3*(i-180))/4);
-  //    c[k+1] = 0; //(unsigned char) (510 - 2*i);
-  //   c[k+2] = 0; //(unsigned char) (i-180)/3;
-  // }
+ // if (i>=180 && i<iMax) {
+//    c[k] = (unsigned char) (210 + (3*(i-180))/4);
+//    c[k+1] = 0; //(unsigned char) (510 - 2*i);
+ //   c[k+2] = 0; //(unsigned char) (i-180)/3;
+ // }
  
  
-  // position , here gradient can be > 1.0 
-  // colors from original image extracted with gimp  : 254/32/19, 249/44/17, 240/68/13
+ // position , here gradient can be > 1.0 
+ // colors from original image extracted with gimp  : 254/32/19, 249/44/17, 240/68/13
   if (position>1.0) {c[k] =  254;  c[k+1] =  32;     c[k+2] = 19;}
-  else if (i>=180) {
-    c[k] = 200 + i/5  ; // from 210  to 254 
-    c[k+1] = 210 - i/2 ; // from 160 to 32
-    c[k+2] = 13;}
+  	else if (i>=180) {
+    		c[k] = 200 + i/5  ; // from 210  to 254 
+    		c[k+1] = 210 - i/2 ; // from 160 to 32
+    		c[k+2] = 13;}
   
     
   
@@ -229,18 +283,18 @@ double complex give_c(int iX, int iY){
 // http://linas.org/art-gallery/escape/ray.html
 double complex GiveGradient(double complex C ){
 
-  double g; // gradient
+  double complex g; // gradient
   int i; // iteration
   double complex Z  = 0.0; // initial value for iteration Z0
   double complex dZ = 1.0; // derivative with respect to c
-  // https://en.wikibooks.org/wiki/Fractals/Iterations_in_the_complex_plane/def_cqp#Derivative_with_respect_to_c 
+ // https://en.wikibooks.org/wiki/Fractals/Iterations_in_the_complex_plane/def_cqp#Derivative_with_respect_to_c 
   
   
   // iteration
   for(i=0;i<IterationMax;i++)
     {
       
-      dZ = 2 * Z * dZ +1.0; // derivative
+      dZ = 2.0 * Z * dZ +1.0; // derivative
       Z=Z*Z+C; // complex quadratic map
       if(cabs(Z)>EscapeRadius) break; // bailout test 
     }
@@ -257,15 +311,39 @@ double complex GiveGradient(double complex C ){
   //m = m/IterationMax; // normalize = map to [0,1]
   
   
-  // the Douady-Hubbard potential is just f = e-m log2 = 2-m
+  // the Douady-Hubbard potential is just f = exp(-m log2) = 2^{-m}
+  // where ^ means superscript:  so a^b = a raised to power b.
+  // This is TeX/LaTex notation (e.g. wikipedia math formulas).
   double potential = pow(2,-m);
-  // 2Df = m |zn Dzn| / (|zn|2 log |zn|)
+
+  // 2Df = 2^{-m} (zbar)_n Dz_n / |z_n|^2 log |z_n|
+  // where zbar is the complex conjugate of z.
+  // This is because:...
+  // |z|^2 = zbar z
+  // and thus d|z| = d sqrt(zbar z) = 0.5 d(zbar z) / sqrt(zbar z)
+  // = 0.5 zbar dz / sqrt(zbar z) = 0.5 zbar dz / |z|
+  //
+  // notice that d zbar = 0  which is why d(zbar z) = zbar dz
+  // this is because zbar only depends on cbar and d cbar = 0 by
+  // orthogonality. So by defintion dc = 1
  
-  g = potential*cabs(Z*dZ)/(cabsZ*cabsZ*log(cabsZ));
-  
+  double complex Zbar = conj(Z);
+  g = potential * Zbar * dZ / (cabsZ*cabsZ*log(cabsZ));
+
   return g;
 }
  
+
+double GiveTurn(double complex z)
+{
+  double argument;
+ 
+  argument = carg(z); //   argument in radians from -pi to pi
+  if (argument<0) argument=argument + TwoPi; //   argument in radians from 0 to 2*pi
+  return argument/TwoPi ; // argument in turns from 0.0 to 1.0
+}
+
+
 
 // find color of pixel 
 int ComputeAndSavePixelColor(int iX, int iY){
@@ -273,13 +351,15 @@ int ComputeAndSavePixelColor(int iX, int iY){
   
   complex double C;
   double complex g; // gradient
-  double gm; // modulus of the gradient 
+  double gm; //  of the gradient 
   int k; // index of the 1D array
    
   C = give_c(iX, iY);
    
   g = GiveGradient(C);
-  gm = cabs(g); 
+
+  // ga = (carg(g) + M_PI)/(2.0*M_PI);
+  gm = cabs(g);
   
   // index of 1D memory array
   k = f(iX, iY);  
@@ -326,7 +406,7 @@ int SaveArray_2_PPM_file (unsigned char A[])
 
   FILE *fp;
   const unsigned int MaxColorComponentValue = 255;	/* color component is coded from 0 to 255 ;  it is 8 bit color file */
-  char *filename = "electric.ppm";
+  char *filename = "landing_b.ppm";
   char *comment = "# ";		/* comment should start with # */
 
   /* save image to the pgm file  */
